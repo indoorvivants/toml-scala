35f4dac5d50459d63494140dc0d14a777dcdaa60
i10416 <ito.yo16uh90616@gmail.com>
2024-12-15 01:40:45 +0900

    improve: pass more and more tests

--- core/src/test/scala/toml/CodecSpec.scala
+++ core/src/test/scala/toml/CodecSpec.scala
@@ -247,6 +247,40 @@ class CodecSpec extends AnyFunSuite with CodecSpecExtras {
       Point(7, "y", 42),
       Point(23, "y", 42)))))
   }
+    test("Inline list of tuples with default values (2)") {
+    val tableList = """points = [ [ 1, "2" ], [ 7 ], [ ] ]"""
+
+    case class Point(x: Int = 23, y: Option[String])
+    case class Root(points: List[Point])
+
+    assert(Toml.parseAs[Root](tableList) == Right(Root(List(
+      Point(1, Some("2")),
+      Point(7, None),
+      Point(23, None)))))
+  }
+
+  test("Inline list of tuples with default values (3)") {
+    val tableList = """points = [ [ 1, "2" ], [ 7 ], [ ] ]"""
+
+    case class Point(x: Option[Int] = Some(23), y: Option[String])
+    case class Root(points: List[Point])
+
+    assert(Toml.parseAs[Root](tableList) == Right(Root(List(
+      Point(Some(1), Some("2")),
+      Point(Some(7), None),
+      Point(Some(23), None)))))
+  }
+
+  test("Inline list of tuples with default values (5)") {
+    val tableList = """points = [ [ 1, "2" ], [ ], [ 3, 4 ] ]"""
+
+    case class Point(x: Int = 23, y: Option[String])
+    case class Root(points: List[Point])
+
+    assert(Toml.parseAs[Root](tableList) ==
+      Left((List("points", "#3", "#2"), "String expected, Num(4) provided")))
+  }
+
 
   test("Nested inline list") {
     val tableList =
