6c1138fdd6127f8599f52b08997f05b31c031de0
i10416 <ito.yo16uh90616@gmail.com>
2024-12-15 03:07:05 +0900

    chore: add note for future developers

--- core/src/test/scala-2/toml/CodecSpec.scala
+++ core/src/test/scala-2/toml/CodecSpec.scala
@@ -1,4 +1,20 @@
 import org.scalatest.funsuite.AnyFunSuite
 import toml._, derivation.auto._
 
-trait CodecSpecExtras {self: CodecSpec => }
+trait CodecSpecExtras {self: CodecSpec =>
+  test("Error handling (6)") {
+    // Despite of the default value, an error must be triggered
+    case class Module(a: Option[Module] = None,
+                      b: List[List[Int]] = List())
+    case class Root(module: Map[String, Module])
+    val toml =
+      """
+        |[module.name.a]
+        |b = [1, 2, 3]
+      """.stripMargin
+    assert(Toml.parseAs[Root](toml) ==
+           Left((
+             List("module", "name", "a", "b", "#1"),
+             "List expected, Num(1) provided")))
+  }
+}
