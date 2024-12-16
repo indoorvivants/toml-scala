1db450f9843830160dfc7c5158a2bb7109dce082
i10416 <ito.yo16uh90616@gmail.com>
2024-12-16 14:19:39 +0900

    feat: derives syntax

---
+++ core/src/main/scala-2/toml/derivation/syntax.scala
@@ -0,0 +1,4 @@
+package toml
+package derivation
+
+trait DerivedSyntax {self: Codec.type => }
---
+++ core/src/main/scala-3/toml/derivation/syntax.scala
@@ -0,0 +1,11 @@
+package toml
+package derivation
+import shapeless3.deriving.*
+
+trait DerivedSyntax {self: Codec.type =>
+    inline def derived[P](using
+      inst: K0.ProductInstances[Codec, P],
+      labelling: Labelling[P],
+      inline d: DefaultParams[P]
+    ): Codec[P] = DerivedProductCodec.codecGen[P]
+}
