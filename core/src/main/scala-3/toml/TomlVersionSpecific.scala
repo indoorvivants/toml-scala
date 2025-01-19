88b5cc633ac5083042aad067302551e01ff0eda5
i10416 <ito.yo16uh90616@gmail.com>
2025-01-20 01:20:10 +0900

    improve: avoid deviation from Scala 2 implementation

--- core/src/main/scala-3/toml/TomlVersionSpecific.scala
+++ core/src/main/scala-3/toml/TomlVersionSpecific.scala
@@ -4,22 +4,24 @@ import toml.derivation.DefaultParams
 import shapeless3.deriving.K0.ProductGeneric
 trait TomlVersionSpecific:
     self:Toml.type =>
-        final def parseAs[T](input: Value.Tbl | String)(
-            using Codec[T], DefaultParams[T]
-        ):Either[Parse.Error, T] = parseAs(input, Set.empty)
+        class CodecHelperGeneric[T]:
+            def apply(table: Value.Tbl)(
+                using codec: Codec[T], D: DefaultParams[T]
+            ): Either[Parse.Error, T] =
+                codec(table, D.defaultParams, 0)
 
-        final def parseAs[T](
-            input: Value.Tbl | String,
-            extensions: Set[Extension]
-        )(using  codec: Codec[T], D: DefaultParams[T]
-        ):Either[Parse.Error, T] = input match
-            case toml: String =>
+            def apply(
+                toml: String,
+                extensions: Set[Extension]
+            )(using codec: Codec[T], D:DefaultParams[T]): Either[Parse.Error, T] =
                 parse(toml, extensions).flatMap(codec(_,D.defaultParams,0))
-            case table: Value.Tbl => codec(
-            table,
-            D.defaultParams,
-            0
-        )
+
+            def apply(
+                toml: String
+            )(using codec: Codec[T], D:DefaultParams[T]): Either[Parse.Error, T] =
+                apply(toml, Set.empty)
+        end CodecHelperGeneric
+
         final class CodecHelperValue[A]:
             def apply(value: Value)(using codec: Codec[A]): Either[Parse.Error, A] =
             codec(value, Map(), 0)
@@ -29,6 +31,8 @@ trait TomlVersionSpecific:
             ): Either[Parse.Error, A] =
             parse(toml, extensions).right.flatMap(codec(_, Map(), 0))
         end CodecHelperValue
+
+        final def parseAs[T]: CodecHelperGeneric[T] = new CodecHelperGeneric[T]
 
         final def parseAsValue[T]: CodecHelperValue[T] = new CodecHelperValue[T]
 
